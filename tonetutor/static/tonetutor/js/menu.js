3c84dabcf1164decc20a4bbd5c106912c57d9e2d
Jivan Amara <Development@JivanAmara.net>
2016-09-13 16:01:04 -0700

    Moved menu javascript from base.html -> menu.js.

---
+++ tonetutor/static/tonetutor/js/menu.js
@@ -0,0 +1,9 @@
+;
+$(document).ready(function() {
+    $('#auth-menu').mouseenter(function() {
+        $('#site-menu').show();; 
+    });
+    $('#auth-menu').mouseleave(function() {
+        $('#site-menu').hide();
+    });
+});
